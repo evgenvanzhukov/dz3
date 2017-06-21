346a6e353e5454672036c7f7cda35dfd918f5a3b
Evgen <evgen-2208@ya.ru>
2017-06-21 13:13:54 +0400

    Add files via upload
    на linq 
    с инициализацией списков в методе класса 
    и использованием decimal

--- Program.cs
+++ Program.cs
@@ -10,55 +10,76 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            List<Price> pricelist = new List<Price>(10);
-            pricelist.Add(new Price {Name = "Огурец", PriceKG = 90 });
-            pricelist.Add(new Price { Name = "Капуста", PriceKG = 80 });
-            pricelist.Add(new Price { Name = "Грибы", PriceKG = 300 });
-            pricelist.Add(new Price { Name = "Помидоры", PriceKG = 130 });
-            pricelist.Add(new Price { Name = "Финики", PriceKG = 100 });
-            pricelist.Add(new Price { Name = "Орехи", PriceKG = 600 });
-            pricelist.Add(new Price { Name = "Мандарины", PriceKG = 150 });
-            pricelist.Add(new Price { Name = "Апельсины", PriceKG = 120 });
-            pricelist.Add(new Price { Name = "Яблоки", PriceKG = 80 });
-            pricelist.Add(new Price { Name = "Черешня", PriceKG = 200 });
+            List<Price> priceList = new List<Price>(10);
+            Price.Initprice(priceList);
 
 
             List<Items> buyList = new List<Items>();
-            buyList.Add(new Items {Name= "Черешня", KG = 2 });
-            buyList.Add(new Items { Name = "Яблоки", KG = 5 });
-            buyList.Add(new Items { Name = "Огурец", KG = 3 });
-            buyList.Add(new Items { Name = "Капуста", KG = 1 });
-            buyList.Add(new Items { Name = "Орехи", KG = 0.5 });
-            buyList.Add(new Items { Name = "Помидоры", KG = 3 });
-            buyList.Add(new Items { Name = "Финики", KG = 1.5 });
-            buyList.Add(new Items { Name = "Апельсины", KG = 4 });
+            Items.initBuyList(buyList);
 
 
-            double allprice = 0;
-            double allves = 0;
-            foreach (Items item in buyList)
-            {
-                var priceitem = (from price in pricelist where price.Name.Equals(item.Name) select price.PriceKG);
-                allprice = allprice + item.KG * priceitem.Sum();
-                //allprice = allprice + item.KG*priceitem; 
-                allves = allves + item.KG;
-            }
-            var avg = allprice / allves;
-            Console.WriteLine(avg);
+            decimal allprice = new Decimal(0.00);
+            decimal allves = new Decimal(0.00);
+
+
+
+            var result = buyList.Join(
+                priceList,
+                __buy => __buy.Name,
+                __price => __price.Name,
+                (__buy, __price) => new
+                {
+                    money = __buy.KG*__price.PriceKG, ves =  __buy.KG
+                });
+            allprice = result.Sum(__m => __m.money);
+            allves = result.Sum(__v => __v.ves);
+
+            decimal avg = (allprice / allves);
+            
+            Console.WriteLine("{0:C}",avg);
             Console.ReadLine();
 
         }
     }
 
 
-    class Price:Items
+    public class Price:Items
     {
-        public int PriceKG { get; set; }
+        public decimal PriceKG { get; set; }
+
+        public static void Initprice(List<Price> priceList)
+        {
+            priceList.Add(new Price { Name = "Огурец", PriceKG = 90 });
+            priceList.Add(new Price { Name = "Капуста", PriceKG = 80 });
+            priceList.Add(new Price { Name = "Грибы", PriceKG = 300 });
+            priceList.Add(new Price { Name = "Помидоры", PriceKG = 130 });
+            priceList.Add(new Price { Name = "Финики", PriceKG = 100 });
+            priceList.Add(new Price { Name = "Орехи", PriceKG = 600 });
+            priceList.Add(new Price { Name = "Мандарины", PriceKG = 150 });
+            priceList.Add(new Price { Name = "Апельсины", PriceKG = 120 });
+            priceList.Add(new Price { Name = "Яблоки", PriceKG = 80 });
+            priceList.Add(new Price { Name = "Черешня", PriceKG = 200 });
+        }
+
     }
 
-    class Items
+    public class Items
     {
         public string Name { get; set; }
-        public double KG { get; set; }
+        public decimal KG { get; set; }
+
+        public static void initBuyList(List<Items> buyList)
+        {
+            buyList.Add(new Items { Name = "Черешня", KG = 2 });
+            buyList.Add(new Items { Name = "Яблоки", KG = 5 });
+            buyList.Add(new Items { Name = "Огурец", KG = 3 });
+            buyList.Add(new Items { Name = "Капуста", KG = 1 });
+            buyList.Add(new Items { Name = "Орехи", KG = 2.50m });
+            buyList.Add(new Items { Name = "Помидоры", KG = 3 });
+            buyList.Add(new Items { Name = "Финики", KG = 1.50m });
+            buyList.Add(new Items { Name = "Апельсины", KG = 4 });
+        }
     }
+
+
 }
